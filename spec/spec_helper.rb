e5d202a5912202463cb8b1146ab93c3475e1b801
Jan Vansteenkiste <jan@vstone.eu>
2013-06-19 12:27:11 +0200

    Deal with lens path for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
+augeas_lenses = File.expand_path(File.join(__FILE__, '../..', 'lib/augeas/lenses'))
+ENV['AUGEAS_LENS_LIB'] = augeas_lenses
+
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
@@ -13,7 +16,7 @@ if Puppet::PUPPETVERSION =~ /^3/
 
   RSpec.configure do |c|
     c.augeas_fixtures = File.join(fixture_path, 'augeas')
-    c.augeas_lensdir = File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib/augeas/lenses')
+    c.augeas_lensdir = augeas_lenses
   end
 end
 
