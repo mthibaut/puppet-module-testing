aa1636167dd99a70ad841ed2af534d0f98240a13
Maarten Thibaut <mthibaut@cisco.com>
2013-09-17 18:56:26 -0400

    [rspec] Second stab: autofs_spec.rb

--- spec/classes/autofs_spec.rb
+++ spec/classes/autofs_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "autofs" do
 
+  let(:facts) {{ :osfamily => 'RedHat' }}
+
   it { should include_class('autofs::package') }
   it { should include_class('autofs::config') }
   it { should include_class('autofs::service') }
