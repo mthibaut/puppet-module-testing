dbd313d2d9bd17857a00e1926eefe209951423d2
Maarten Thibaut <mthibaut@cisco.com>
2013-09-17 19:21:03 -0400

    [rspec] Third stab: autofs_spec.rb

--- spec/classes/autofs_spec.rb
+++ spec/classes/autofs_spec.rb
@@ -2,10 +2,29 @@ require 'spec_helper'
 
 describe "autofs" do
 
-  let(:facts) {{ :osfamily => 'RedHat' }}
+  describe "on osfamily RedHat" do
+    let(:facts) {{ :osfamily => 'RedHat' }}
 
-  it { should include_class('autofs::package') }
-  it { should include_class('autofs::config') }
-  it { should include_class('autofs::service') }
+    it { should include_class('autofs::package') }
+    it { should include_class('autofs::config') }
+    it { should include_class('autofs::service') }
 
+    it do
+      should contain_package('autofs')
+      should contain_service('autofs') \
+             .with_ensure('running')   \
+             .with_alias('autofs')
+    end
+
+  end
+
+  describe "on osfamily Debian" do
+    let(:facts) {{ :osfamily => 'Debian' }}
+
+    it do
+      expect {
+        should include_class('autofs::package')
+      }.to raise_error(Puppet::Error, /osfamily not supported:/)
+    end
+  end
 end
